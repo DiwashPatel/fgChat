793d3abc71a190ecfb7d714eb80ac4f97d0ace07
Diwash Patel <diwashpatel@dpmac.local>
2026-09-08 12:50:44 -0500

    allows broadcasting to all clients

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ import threading
 HOST = "127.0.0.1"
 PORT = 5000
 
+clients = []
+
+def broadcast(message):
+    for client in clients:
+        client.sendall(message.encode()) # since, message was decoded (it became from bytes to str). So again making it bytes.
+
 def handle_client(client_socket, address):
 
     print(f"{address} connected")
@@ -18,6 +24,7 @@ def handle_client(client_socket, address):
         message = data.decode()
 
         print(f"{address}: {message}")
+        broadcast(message)
 
     client_socket.close()
 
@@ -29,17 +36,21 @@ server.listen()
 print(f"Server started...")
 print(f"\n Details: IP: {HOST} Port: {PORT} ProcessID: {os.getpid()}")
 
+try:
+    while True:
+        client_socket, address = server.accept()
 
-while True:
-    client_socket, address = server.accept()
+        clients.append(client_socket)
 
-    thread = threading.Thread(
-        target=handle_client,
-        args=(client_socket, address)
-    )
+        thread = threading.Thread(
+            target=handle_client,
+            args=(client_socket, address)
+        )
 
-    thread.start()
-
-
-
-    
+        thread.start()
+except KeyboardInterrupt:
+    print(f"\nStopping Server: keyboard interrupt occured.")
+finally:
+    for client in clients:
+        client.close()
+    server.close()
