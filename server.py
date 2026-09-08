5977852cb6cf2a1d69f501d8cf85de85f7b0f2c6
Diwash Patel <diwashpatel@dpmac.local>
2026-09-08 15:45:20 -0500

    removes dead clients by locking first

--- server.py
+++ server.py
@@ -7,9 +7,21 @@ PORT = 5000
 
 clients = []
 
+# Creating a Lock Object. If one thread tries to remove a client and other tries to broadcast, it will cause list changed during iteration error.
+clients_lock = threading.Lock()
+
+# Also, we can't change the list during iteration, so removing the client in case of exception is also chaning the list. so better to list all dead sockets and then later remove.
 def broadcast(message):
-    for client in clients:
-        client.sendall(message.encode()) # since, message was decoded (it became from bytes to str). So again making it bytes.
+    dead_clients = []
+    with clients_lock:
+        for client in clients[:]:
+            try:
+                client.sendall(message.encode()) # since, message was decoded (it became from bytes to str). So again making it bytes.
+            except Exception:
+                dead_clients.append(client)
+
+        for dead_client in dead_clients:
+            clients.remove(dead_client) # No problem, since client sockets are always unique.
 
 def handle_client(client_socket, address):
 
@@ -24,7 +36,7 @@ def handle_client(client_socket, address):
         message = data.decode()
 
         print(f"{address}: {message}")
-        broadcast(message)
+        broadcast(message) # We can also send the data.
 
     client_socket.close()
 
@@ -33,6 +45,7 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 
 server.listen()
+
 print(f"Server started...")
 print(f"\n Details: IP: {HOST} Port: {PORT} ProcessID: {os.getpid()}")
 
